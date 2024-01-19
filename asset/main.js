082db0aa40108cd139367d376a2ab72b64b32df4
林墨白 <127305127+LMB520@users.noreply.github.com>
2024-01-19 14:41:59 +0800

    代码错误
    文件里面的代码不知道为啥翻译成中文了，哈哈哈哈哈哈哈哈哈哈哈

--- asset/main.js
+++ asset/main.js
@@ -1,37 +1,45 @@
-var 蒂姆=1;
-setInterval("tim++",10);
-var autourl=新的数组();
-为(我=0；i<=6；i++){
-autourl[我]=$('.speedlist p').情商(我).找到(‘A’).attr("href");
+var tim = 1;
+
+setInterval(function() {
+  tim++;
+}, 10);
+
+var autourl = [];
+
+for (var i = 0; i <= 6; i++) {
+  autourl[i] = $('.speedlist p').eq(i).find('a').attr("href");
 }
-功能 自动(URL){
-	var str=tim；
-控制台。日志(str);
-	如果(URL==autourl[0]){
-		$('#lineMs0').超文本标记语言(str+'MS')
-	}
-	如果(URL==autourl[1]){
-		$('#lineMs1').超文本标记语言(str+'MS')
-	}
-	如果(URL==autourl[2]){
-		$('#lineMs2').超文本标记语言(str+'MS')
-	}
-	如果(URL==autourl[3]){
-		$('#lineMs3').超文本标记语言(str+'MS')
-	}
-	如果(URL==autourl[4]){
-		$('#lineMs4').超文本标记语言(str+'MS')
-	}
-	如果(URL==autourl[5]){
-		$('#lineMs5').超文本标记语言(str+'MS')
-	}
-	如果(URL==autourl[6]){
-		$('#lineMs6').超文本标记语言(str+'MS')
-	}
+
+function auto(url) {
+  var str = tim;
+  console.log(str);
+  if (url == autourl[0]) {
+    $('#lineMs0').html(str + 'ms');
+  }
+  if (url == autourl[1]) {
+    $('#lineMs1').html(str + 'ms');
+  }
+  if (url == autourl[2]) {
+    $('#lineMs2').html(str + 'ms');
+  }
+  if (url == autourl[3]) {
+    $('#lineMs3').html(str + 'ms');
+  }
+  if (url == autourl[4]) {
+    $('#lineMs4').html(str + 'ms');
+  }
+  if (url == autourl[5]) {
+    $('#lineMs5').html(str + 'ms');
+  }
+  if (url == autourl[6]) {
+    $('#lineMs6').html(str + 'ms');
+  }
 }
-功能 跑(){
-	为(var 跨度=0；span<autourl。长度；span++){
-文件。写("<img src="+autourl[跨度]+"/"+数学。随机()+"width=1height=1onerror=auto('"+autourl[跨度]+"')style='display:none'>");
-	}
+
+function run() {
+  for (var span = 0; span < autourl.length; span++) {
+    document.write("<img src=" + autourl[span] + "/" + Math.random() + " width=1 height=1 onerror=\"auto('" + autourl[span] + "')\" style='display:none'>");
+  }
 }
-跑()
+
+run();
